b41b62d62fe5739df485c5d2ecad4aef9f5521ff
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2025-07-11 14:13:48 +0000

    add selection sort

---
+++ selection_sort.c
@@ -0,0 +1,12 @@
+#include "sorting.h"
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        if (min_idx != i)
+            swap(&arr[i], &arr[min_idx]);
+    }
+}
